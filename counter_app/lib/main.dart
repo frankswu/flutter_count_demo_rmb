217b8199c72226ba4f09cfa62326996534dd898d
FranksWu <franks.wu@guanaitong.com>
2021-07-04 23:17:12 +0800

    add bloc demo

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,45 +1,58 @@
 import 'package:flutter/material.dart';
-// ignore: import_of_legacy_library_into_null_safe
-import 'package:provider/provider.dart';
 
-void main() {
-  var flutterProviderApp = FlutterProviderApp(
-    title: "Flutter Provider Demo",
-  );
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-  runApp(
-    MultiProvider(
-      providers: [
-        ChangeNotifierProvider(
-          create: (BuildContext context) => CountModel(),
-        ),
-        ChangeNotifierProvider(
-          create: (BuildContext context) => ThemeModel(),
-        )
-      ],
-      child: flutterProviderApp,
-    ),
-  );
+
+class MyBlocDelegate extends BlocDelegate {
+  @override
+  void onEvent(Bloc bloc, Object event) {
+    print(event);
+    super.onEvent(bloc, event);
+  }
+
+  @override
+  void onTransition(Bloc bloc, Transition transition) {
+    print(transition);
+    super.onTransition(bloc, transition);
+  }
+
+  @override
+  void onError(Bloc bloc, Object error, StackTrace stackTrace) {
+    print(error);
+    super.onError(bloc, error, stackTrace);
+  }
 }
 
-class FlutterProviderApp extends StatelessWidget {
+void main() {
+  BlocSupervisor.delegate = MyBlocDelegate();
+  runApp(FlutterBlocApp(
+    title: "Flutter Bloc Demo",
+  ));
+}
+
+class FlutterBlocApp extends StatelessWidget {
   final String title;
 
-  const FlutterProviderApp({Key? key, required this.title}) : super(key: key);
+  const FlutterBlocApp({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Consumer<ThemeModel>(builder: (context, theme, child) {
-      return MaterialApp(
-        title: title,
-        theme: theme.themeData,
-        home: HomePage(title: title),
-      );
-    });
+    return BlocProvider(
+      create: (_) => ThemeBloc(),
+      child: BlocBuilder<ThemeBloc, ThemeData>(builder: (_, theme) {
+        return MaterialApp(
+          title: title,
+          theme: theme,
+          home: BlocProvider(
+              create: (_) => CountBloc(), child: HomePage(title: title)),
+        );
+      }),
+    );
   }
 }
 
 class HomePage extends StatelessWidget {
+  //
   final String title;
 
   const HomePage({Key? key, required this.title}) : super(key: key);
@@ -48,8 +61,7 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     var floatingActionButton1 = FloatingActionButton(
       // Attach the `callback` to the `onPressed` attribute
-      onPressed: () =>
-          Provider.of<CountModel>(context, listen: false).incrementCount(),
+      onPressed: () => context.bloc<CountBloc>().add(CountEvent.increment),
       tooltip: 'Increment Count',
       child: Icon(Icons.add),
     );
@@ -58,22 +70,22 @@ class HomePage extends StatelessWidget {
       appBar: AppBar(
         title: Text(title),
       ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Consumer<CountModel>(builder: (context, count, child) {
-              return Text(
-                '${count._count}',
+      body: BlocBuilder<CountBloc, int>(builder: (_, count) {
+        return Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'You have pushed the button this many times:',
+              ),
+              Text(
+                '$count',
                 style: Theme.of(context).textTheme.display1,
-              );
-            }),
-          ],
-        ),
-      ),
+              ),
+            ],
+          ),
+        );
+      }),
       floatingActionButton: Column(
         crossAxisAlignment: CrossAxisAlignment.end,
         mainAxisAlignment: MainAxisAlignment.end,
@@ -85,22 +97,15 @@ class HomePage extends StatelessWidget {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              onPressed: () => Provider.of<CountModel>(context, listen: false)
-                  .decreaseCount(),
+              onPressed: () =>
+                  context.bloc<CountBloc>().add(CountEvent.decrement),
               child: Icon(Icons.remove),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              //// Provider.of is another way to access the model object held
-              // by an ancestor Provider. By default, even this listens to
-              // changes in the model, and rebuilds the whole encompassing widget
-              // when notified.
-              // 下面使用 `listen: false`，我们禁用了默认行为。在这里我们只是调用函数，我们不需要关心当前值
-              // 没有`listen: false` ，当ThemeModel 被通知改变的时候，会重刷新整个FlutterProviderApp
-              onPressed: () =>
-                  Provider.of<ThemeModel>(context, listen: false).toggleTheme(),
+              onPressed: () => context.bloc<ThemeBloc>().add(ThemeEvent.toggle),
               child: Icon(Icons.brightness_6),
             ),
           ),
@@ -110,27 +115,40 @@ class HomePage extends StatelessWidget {
   }
 }
 
-class CountModel extends ChangeNotifier {
-  int _count = 0;
+enum CountEvent { increment, decrement }
 
-  void incrementCount() {
-    _count++;
-    notifyListeners();
-  }
+class CountBloc extends Bloc<CountEvent, int> {
+  @override
+  int get initialState => 0;
 
-  void decreaseCount() {
-    _count--;
-    notifyListeners();
+  @override
+  Stream<int> mapEventToState(CountEvent event) async* {
+    switch (event) {
+      case CountEvent.increment:
+        yield state + 1;
+        break;
+      case CountEvent.decrement:
+        yield state - 1;
+        break;
+      default:
+        throw Exception("no");
+    }
   }
 }
 
-class ThemeModel extends ChangeNotifier {
-  ThemeData themeData = ThemeData.dark();
+enum ThemeEvent { toggle }
 
-  void toggleTheme() {
-    themeData =
-        (themeData == ThemeData.dark()) ? ThemeData.light() : ThemeData.dark();
-    notifyListeners();
+class ThemeBloc extends Bloc<ThemeEvent, ThemeData> {
+  @override
+  ThemeData get initialState => ThemeData.light();
+
+  @override
+  Stream<ThemeData> mapEventToState(event) async* {
+    switch (event) {
+      case ThemeEvent.toggle:
+        yield state == ThemeData.dark() ? ThemeData.light() : ThemeData.dark();
+        break;
+    }
   }
 }
 
