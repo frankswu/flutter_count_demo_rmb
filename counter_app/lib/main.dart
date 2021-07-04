29aa17d78b68962965afd6a12a041be29c49a61c
FranksWu <franks.wu@guanaitong.com>
2021-07-04 22:43:25 +0800

    add provider demo

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,95 +1,62 @@
 import 'package:flutter/material.dart';
+// ignore: import_of_legacy_library_into_null_safe
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(FlutterNormalApp(
-    title: "Flutter Normal Demo",
-  ));
+  var flutterProviderApp = FlutterProviderApp(
+    title: "Flutter Provider Demo",
+  );
+
+  runApp(
+    MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          create: (BuildContext context) => CountModel(),
+        ),
+        ChangeNotifierProvider(
+          create: (BuildContext context) => ThemeModel(),
+        )
+      ],
+      child: flutterProviderApp,
+    ),
+  );
 }
 
-
-class FlutterNormalApp extends StatefulWidget {
-
+class FlutterProviderApp extends StatelessWidget {
   final String title;
 
-  const FlutterNormalApp({Key? key, required this.title}) : super(key: key);
-
-
-  @override
-  State<StatefulWidget> createState() {
-    return _FlutterNormalAppState(title);
-  }
-
-}
-
-class _FlutterNormalAppState extends State<FlutterNormalApp> {
-
-  final String title;
-  
-  ThemeData themeData = ThemeData.dark();
-
-  _FlutterNormalAppState(this.title);
-
-  void toggleTheme() {
-    setState(() {
-      themeData = (themeData == ThemeData.dark()) ? ThemeData.light() : ThemeData.dark();
-      //return themeData;
-    });
-  }
+  const FlutterProviderApp({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: title,
-      theme: this.themeData,
-      home: HomePage(view: this, title: title),
-    );
+    return Consumer<ThemeModel>(builder: (context, theme, child) {
+      return MaterialApp(
+        title: title,
+        theme: theme.themeData,
+        home: HomePage(title: title),
+      );
+    });
   }
-
 }
-// 有状态 页面
-class HomePage extends StatefulWidget {
+
+class HomePage extends StatelessWidget {
   final String title;
-  final _FlutterNormalAppState view;
 
-  const HomePage({Key? key, required this.title, required this.view}) : super(key: key);
-
-
-  @override
-  State<StatefulWidget> createState() => _HomePageState(view);
-}
-
-class _HomePageState extends State<HomePage> {
-
-  int _count = 0;
-  final _FlutterNormalAppState view;
-
-  _HomePageState(this.view);
-
-  get themeData => null;
-
-  void _incrementCount() {
-    setState(() {
-      _count++;
-    });
-  }
-
-  void _decreaseCount() {
-    setState(() {
-      _count--;
-    });
-  }
+  const HomePage({Key? key, required this.title}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     var floatingActionButton1 = FloatingActionButton(
       // Attach the `callback` to the `onPressed` attribute
-      onPressed: _incrementCount,
+      onPressed: () =>
+          Provider.of<CountModel>(context, listen: false).incrementCount(),
       tooltip: 'Increment Count',
       child: Icon(Icons.add),
     );
+
     return Scaffold(
       appBar: AppBar(
-        title: Text(widget.title),
+        title: Text(title),
       ),
       body: Center(
         child: Column(
@@ -98,10 +65,12 @@ class _HomePageState extends State<HomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_count',
-              style: Theme.of(context).textTheme.display1,
-            ),
+            Consumer<CountModel>(builder: (context, count, child) {
+              return Text(
+                '${count._count}',
+                style: Theme.of(context).textTheme.display1,
+              );
+            }),
           ],
         ),
       ),
@@ -116,20 +85,52 @@ class _HomePageState extends State<HomePage> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              onPressed: _decreaseCount,
+              onPressed: () => Provider.of<CountModel>(context, listen: false)
+                  .decreaseCount(),
               child: Icon(Icons.remove),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              onPressed: view.toggleTheme,
+              //// Provider.of is another way to access the model object held
+              // by an ancestor Provider. By default, even this listens to
+              // changes in the model, and rebuilds the whole encompassing widget
+              // when notified.
+              // 下面使用 `listen: false`，我们禁用了默认行为。在这里我们只是调用函数，我们不需要关心当前值
+              // 没有`listen: false` ，当ThemeModel 被通知改变的时候，会重刷新整个FlutterProviderApp
+              onPressed: () =>
+                  Provider.of<ThemeModel>(context, listen: false).toggleTheme(),
               child: Icon(Icons.brightness_6),
             ),
           ),
         ],
       ),
     );
+  }
+}
+
+class CountModel extends ChangeNotifier {
+  int _count = 0;
+
+  void incrementCount() {
+    _count++;
+    notifyListeners();
+  }
+
+  void decreaseCount() {
+    _count--;
+    notifyListeners();
+  }
+}
+
+class ThemeModel extends ChangeNotifier {
+  ThemeData themeData = ThemeData.dark();
+
+  void toggleTheme() {
+    themeData =
+        (themeData == ThemeData.dark()) ? ThemeData.light() : ThemeData.dark();
+    notifyListeners();
   }
 }
 
--- counter_app/test/widget_test.dart
+++ counter_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:counter_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(FlutterNormalApp( title: 'Test',));
+    await tester.pumpWidget(FlutterProviderApp(title: 'Test'));
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
