210fa0d34f78db8840ad81e709a8eafcbb38e05f
FranksWu <franks.wu@guanaitong.com>
2021-07-07 09:33:46 +0800

    add normal rxdart  demo

--- counter_app/lib/main.dart
+++ counter_app/lib/main.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -24,24 +26,30 @@ class FlutterNormalApp extends StatefulWidget {
 class _FlutterNormalAppState extends State<FlutterNormalApp> {
 
   final String title;
-  
   ThemeData themeData = ThemeData.dark();
+  
+
+  final StreamController<int> _streamThemeDataController = StreamController<int>();
 
   _FlutterNormalAppState(this.title);
 
-  void toggleTheme() {
-    setState(() {
-      themeData = (themeData == ThemeData.dark()) ? ThemeData.light() : ThemeData.dark();
-      //return themeData;
-    });
+// TODO fix bug not toggle theme
+  ThemeData toggleTheme() {
+    return   (themeData == ThemeData.dark()) ? ThemeData.light() : ThemeData.dark();
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: title,
-      theme: this.themeData,
-      home: HomePage(view: this, title: title),
+    return StreamBuilder(
+      stream: _streamThemeDataController.stream,
+      initialData: 0,
+      builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+        return MaterialApp(
+              title: title,
+              theme: toggleTheme(),
+              home: HomePage(view: this, title: title),
+            );      
+      }
     );
   }
 
@@ -63,27 +71,27 @@ class _HomePageState extends State<HomePage> {
   int _count = 0;
   final _FlutterNormalAppState view;
 
+  final StreamController<int> _streamController = StreamController<int>();
+
   _HomePageState(this.view);
 
   get themeData => null;
 
-  void _incrementCount() {
-    setState(() {
-      _count++;
-    });
-  }
 
-  void _decreaseCount() {
-    setState(() {
-      _count--;
-    });
+  @override
+  void dispose() {
+    // 生命周期销毁的时候，_streamController一起销毁
+    _streamController.close();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
     var floatingActionButton1 = FloatingActionButton(
       // Attach the `callback` to the `onPressed` attribute
-      onPressed: _incrementCount,
+              onPressed: () {
+                _streamController.sink.add(++_count);
+              },
       tooltip: 'Increment Count',
       child: Icon(Icons.add),
     );
@@ -98,11 +106,15 @@ class _HomePageState extends State<HomePage> {
             Text(
               'You have pushed the button this many times:',
             ),
-            Text(
-              '$_count',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+            // Text 控件需要使用StreamBuilder 包装一层才能有相应能力
+            StreamBuilder<int>(
+              stream: _streamController.stream,
+              initialData: _count,
+              builder: (BuildContext context, AsyncSnapshot<int> snapshot) {
+                return   Text('${snapshot.data}', style: Theme.of(context).textTheme.display1);
+              },
+            )
+,          ],
         ),
       ),
       floatingActionButton: Column(
@@ -116,14 +128,18 @@ class _HomePageState extends State<HomePage> {
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              onPressed: _decreaseCount,
+              onPressed: () {
+                _streamController.sink.add(--_count);
+              },
               child: Icon(Icons.remove),
             ),
           ),
           Padding(
             padding: const EdgeInsets.symmetric(vertical: 5.0),
             child: FloatingActionButton(
-              onPressed: view.toggleTheme,
+              onPressed: () {
+                view._streamThemeDataController.sink.add(0);
+              },
               child: Icon(Icons.brightness_6),
             ),
           ),
